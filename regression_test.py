246993f34184dc9024587b943f1977e4cce81795
Bibek Koirala <kbibek371@gmail.com>
2024-01-25 00:16:55 -0500

    regression_test changed

--- regression_test.py
+++ regression_test.py
@@ -1,5 +1,4 @@
 import torch
-import pytest
 from regression import fit_regression_model
 
 
@@ -60,7 +59,7 @@ def get_train_data(dim=1):
 
 def test_fit_regression_model_1d():
     X, y = get_train_data(dim=1)
-    model, loss = fit_regression_model(X, y)
+    _, loss = fit_regression_model(X, y)
     print(loss)
 
     assert loss.item() < 4321,  " loss too big"
@@ -77,9 +76,11 @@ def test_fit_and_predict_regression_model_1d():
     model, loss = fit_regression_model(X, y)
     X_test = torch.tensor([[20.], [15.], [10.]])
     y_pred = model(X_test)
-    assert ((y_pred - torch.tensor([[1252.3008],
+    
+    assert isinstance(y_pred, torch.Tensor), "y_pred is not a Tensor"
+    assert (torch.abs(y_pred - torch.tensor([[1252.3008],
                                     [939.9971],
-                                    [627.6935]])).abs() < 2).all(), " y_pred is not correct"
+                                    [627.6935]])) < 2).all(), f" y_pred is not correct"
     assert y_pred.shape == (3, 1), " y_pred shape is not correct"
 
 
@@ -89,14 +90,14 @@ def test_fit_and_predict_regression_model_2d():
     X_test = torch.tensor([[20., 2.], [15., 3.], [10., 4.]])
     y_pred = model(X_test)
 
-    assert ((y_pred - torch.tensor([[1191.9037],
+    assert (torch.abs(y_pred - torch.tensor([[1191.9037],
                                     [943.9369],
-                                    [695.9700]])).abs() < 2).all(), " y_pred is not correct"
+                                    [695.9700]])) < 2).all(), " y_pred is not correct"
     assert y_pred.shape == (3, 1), " y_pred shape is not correct"
 
 
-if __name__ == "__main__":
-    test_fit_regression_model_1d()
-    test_fit_regression_model_2d()
-    test_fit_and_predict_regression_model_1d()
-    test_fit_and_predict_regression_model_2d()
+# if __name__ == "__main__":
+#     test_fit_regression_model_1d()
+#     test_fit_regression_model_2d()
+#     test_fit_and_predict_regression_model_1d()
+#     test_fit_and_predict_regression_model_2d()
